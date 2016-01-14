b796a4b1ef6da7abc8e3a4c85867f54a5e3351aa
Ed Jones <ed@errorstudio.co.uk>
2016-01-14 21:59:24 +0000

    A working price list sync. whoop.

--- lib/rooftop/spektrix_sync/lib/instance_sync.rb
+++ lib/rooftop/spektrix_sync/lib/instance_sync.rb
@@ -11,11 +11,16 @@ module Rooftop
       end
 
       def sync
+        # This is a bit of a hack: we have to create a new instance and assign all the attributes from the old one, so it has an _event_id param.
+        unless @rooftop_instance.new?
+          @rooftop_instance = @rooftop_event.instances.build(@rooftop_instance.attributes)
+          instance_updated = true
+        end
         update_meta_attributes
         update_availability
         update_on_sale
         if @rooftop_instance.save!
-          @logger.debug("Saved instance #{@rooftop_instance.id}")
+          @logger.debug("#{instance_updated ? "Updated" : "Created"} instance #{@rooftop_instance.id}")
         end
       end
 
--- lib/rooftop/spektrix_sync/lib/price_list_sync.rb
+++ lib/rooftop/spektrix_sync/lib/price_list_sync.rb
@@ -5,37 +5,71 @@ module Rooftop
       def initialize
         @spektrix_price_lists = Spektrix::Tickets::PriceList.all.to_a
         @rooftop_price_lists = Rooftop::Events::PriceList.all.to_a
-        @rooftop_price_bands = Rooftop::Events::PriceBand.all.to_a
         @rooftop_ticket_types = Rooftop::Events::TicketType.all.to_a
+        @rooftop_price_bands = Rooftop::Events::PriceBand.all.to_a
       end
 
       def run
-        @spektrix_price_lists.each do |list|
+        @spektrix_price_lists.each do |spektrix_price_list|
+          # Don't bother syncing a price list where none of the prices have bands.
+          if spektrix_price_list.prices.select {|p| !p.band.nil?}.empty?
+            puts "Skipping price list #{spektrix_price_list.id}"
+            next
+          end
+
           # find or create a price list
-          rooftop_price_list = @rooftop_price_lists.find {|l| l.title == list.id.to_s} || Rooftop::Events::PriceList.new(title: list.id)
+          @rooftop_price_list = @rooftop_price_lists.find {|l| l.meta_attributes[:spektrix_id] == spektrix_price_list.id.to_s} || Rooftop::Events::PriceList.new(title: spektrix_price_list.id, meta_attributes: {})
+          @rooftop_price_list.meta_attributes[:spektrix_id] = spektrix_price_list.id
 
+          new_price_list = @rooftop_price_list.new?
           # save the price list to rooftop
-          rooftop_price_list.save
-
-          # get an array of known prices
-          rooftop_prices = rooftop_price_list.prices.to_a
-
-          # iterate over each spektrix price which looks like this:
-          #  => #<Spektrix::Tickets::Price band=nil, is_band_default="true", price="32.00", ticket_type=#<Spektrix::Tickets::Type(ticket-types/201) name="Standard" id="201">>
-          list.prices.each do |price|
-            # skip ones without a band
-            next if price.band.nil?
-
-            # Find the ticket type - we need the id in a sec
-            rooftop_ticket_type = @rooftop_ticket_types.find {|t| t.title == price.ticket_type.name}
-            rooftop_price = rooftop_prices.find {|p| }
-
+          if @rooftop_price_list.save!
+            puts "#{new_price_list ? "Created" : "Updated"} price list #{spektrix_price_list.id}"
+            sync_prices(spektrix_price_list, @rooftop_price_list)
           end
         end
       end
 
       private
 
+      def sync_prices(spektrix_price_list, rooftop_price_list)
+        spektrix_price_list.prices.each_with_index do |spektrix_price, i |
+          puts "syncing price #{i+1} / #{spektrix_price_list.prices.count}"
+          # skip ones without a band
+          if spektrix_price.band.nil?
+            puts "skipping #{spektrix_price.name}"
+            next
+          end
+          current_rooftop_price = find_rooftop_price(rooftop_price_list, spektrix_price)
+          # because updating a price is hard with a nested resource, we'll destroy and recreate with the same settings
+          new_price = rooftop_price_list.prices.build
+          if current_rooftop_price
+            new_price.assign_attributes(current_rooftop_price.attributes)
+          end
+          new_price.meta_attributes = {
+            is_band_default: (spektrix_price.is_band_default == "true"),
+            ticket_type_id: find_rooftop_ticket_type(spektrix_price).id,
+            price_band_id: find_rooftop_price_band(spektrix_price).id,
+            ticket_price: spektrix_price.price.to_f
+          }
+          new_price.title = "#{spektrix_price.band.name} (£#{new_price.meta_attributes[:ticket_price]})"
+          if new_price.save!
+            puts "saved #{new_price.meta_attributes[:ticket_price]}: #{spektrix_price.band.name}"
+          end
+        end
+      end
+
+      def find_rooftop_price(rooftop_price_list,spektrix_price)
+        rooftop_price_list.prices.to_a.find {|p| p.meta_attributes[:spektrix_id] == spektrix_price.id}
+      end
+
+      def find_rooftop_ticket_type(spektrix_price)
+        @rooftop_ticket_types.find {|t| t.title == spektrix_price.ticket_type.name}
+      end
+
+      def find_rooftop_price_band(spektrix_price)
+        @rooftop_price_bands.find {|b| b.title == spektrix_price.band.name}
+      end
 
 
     end
